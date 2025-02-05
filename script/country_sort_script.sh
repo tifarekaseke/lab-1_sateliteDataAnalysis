29e259221b4eb0262f1f144141ad9f5ded0d4b80
root <tifarekaseke>
2025-02-05 02:38:56 -0800

    corrected script with headings

--- script/country_sort_script.sh
+++ script/country_sort_script.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# This script will sort the data of a specific country, South Africa, from the satelite data
+# This script extracts and sorts satellite data for South Africa, retaining the header.
 
-# Define file file paths
+# Define file paths
 south_africa_data="../analyzed_data/humidity_data_south_africa.csv"
 satelite_location="../raw_data/satelite_temperature_data.csv"
 
+# Extract the header
+header=$(head -n 1 "$satelite_location")
+
 # Extract South Africa data and sort by column 4 in descending order
-awk -F, '$1 == "South Africa" {print}' $satelite_location | sort -t, -k4 -nr > $south_africa_data
+awk -F, 'NR==1 || $1 == "South Africa"' "$satelite_location" | sort -t, -k4 -nr > "$south_africa_data"
+
+# Ensure the header remains at the top
+sed -i "1s/.*/$header/" "$south_africa_data"
 
